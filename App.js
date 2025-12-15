43e37f08df58c2cb75db8ebd3545f44089af5a72
Roshinicoding <wathsalroshini42@gmail.com>
2025-12-15 08:56:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Fixly</Text>
+      <Image
+        source={require('./assets/icon.png')}
+        style={styles.logo}
+      />
+      <Text style={styles.subtitle}>Your Home Hero</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#349cc5ff', // blue sky
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 10,
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    color: '#fff',
+  },
+});
